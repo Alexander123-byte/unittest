358c45c4ce2c18561afd88d550ebc9848fe6906f
Alexander <san.juckov2017@yandex.ru>
2023-12-09 17:33:25 +0300

    Fix test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,7 +1,4 @@
 import unittest
-import sys
-
-sys.path.append("C:/Users/User/PycharmProjects/homework_22_11_unittest/unittest_proj/utils/arrs1.py")
 from unittest_proj.utils import arrs1
 
 
@@ -14,3 +11,5 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs1.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs1.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs1.my_slice([], 1), [])
+        self.assertEqual(arrs1.my_slice([1, 2, 3], 0, 1), [1])
